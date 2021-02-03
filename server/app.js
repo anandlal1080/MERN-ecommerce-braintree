f934e7e064e54ea8b6bdb623d5e5c5e79df128b7
Allan Nandlal <72092423+anandlal1080@users.noreply.github.com>
2021-02-03 12:49:20 -0700

    created user model

--- server/app.js
+++ server/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 const userRoutes = require("./routes/user.js");
@@ -16,6 +19,10 @@ mongoose
   })
   .then(() => console.log("MongoAtlas Connected"));
 
+//
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
 // routes middleware
 
 app.use("/api", userRoutes);
