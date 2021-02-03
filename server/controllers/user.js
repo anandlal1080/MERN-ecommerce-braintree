9b17e3a36da8ca1810697360d271f37f0f7b70c3
Allan Nandlal <72092423+anandlal1080@users.noreply.github.com>
2021-02-03 13:41:46 -0700

    created user signin

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,4 +1,6 @@
 const User = require("../models/user.js");
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
 const { errorHandler } = require("../helpers/dbErrorHandler.js");
 
 exports.signup = (req, res) => {
@@ -15,5 +17,31 @@ exports.signup = (req, res) => {
     res.json({
       user,
     });
+  });
+};
+
+exports.signin = (req, res) => {
+  // find user based one email
+  const { email, password } = req.body;
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User with that email does not exist. Please signup",
+      });
+    }
+    // if user is found make sure the email and password match
+    // create authenticate method in user model
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        error: "Email and password do not match",
+      });
+    }
+    // genreate a signed token with user id and secret
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+    // persist the token at 't' in cookie with expiry date
+    res.cookie("t", token, { expire: new Date() + 9999 });
+    // return response with user and token to frotnend client
+    const { _id, name, email, role } = user;
+    return res.json({ token, user: { _id, name, email, role } });
   });
 };
