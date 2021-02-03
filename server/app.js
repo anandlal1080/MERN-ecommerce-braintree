4a2d2efef3b9f6fb5374bdb035221f3e76026ee3
Allan Nandlal <72092423+anandlal1080@users.noreply.github.com>
2021-02-03 11:48:06 -0700

    created routes and controllers

--- server/app.js
+++ server/app.js
@@ -1,10 +1,24 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 require("dotenv").config();
 
-app.get("/", (req, res) => {
-  res.send("Hello from Node ");
-});
+const userRoutes = require("./routes/user.js");
+
+const app = express();
+
+// DB
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoAtlas Connected"));
+
+// routes middleware
+
+app.use("/api", userRoutes);
 
 const port = process.env.PORT || 8000;
 
