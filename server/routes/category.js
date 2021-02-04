beaa11661cb3cf6374b463cf4739b5bbe3cf5bd1
Allan Nandlal <72092423+anandlal1080@users.noreply.github.com>
2021-02-04 15:19:16 -0700

    added category routes

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, categoryById, read } = require("../controllers/category.js");
+const {
+  create,
+  categoryById,
+  read,
+  update,
+  remove,
+  list,
+} = require("../controllers/category.js");
 const { requireSignin, isAdmin, isAuth } = require("../controllers/auth.js");
 const { userById } = require("../controllers/user.js");
 
 router.get("/category/:categoryId", read);
 
 router.post("/category/create/:userId", requireSignin, isAdmin, isAuth, create);
+router.put(
+  "/category/:categoryId/:userId",
+  requireSignin,
+  isAdmin,
+  isAuth,
+  update
+);
+router.delete(
+  "/category/:categoryId/:userId",
+  requireSignin,
+  isAdmin,
+  isAuth,
+  remove
+);
+
+router.get("/categories", list);
 
 router.param("categoryId", categoryById);
 router.param("userId", userById);
