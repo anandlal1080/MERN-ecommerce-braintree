0867d7c089673c30cc81d580cd5aa1be9a4fbb86
Allan Nandlal <72092423+anandlal1080@users.noreply.github.com>
2021-02-03 18:34:14 -0700

    added product create and read

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controllers/product.js");
+const { create, productById, read } = require("../controllers/product.js");
 const { requireSignin, isAdmin, isAuth } = require("../controllers/auth.js");
 const { userById } = require("../controllers/user.js");
 
+router.get("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAdmin, isAuth, create);
 
 router.param("userId", userById);
+router.param("productId", productById);
 
 module.exports = router;
